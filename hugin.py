0e0b6974a831abbf636bbd6aef006569836715d2
phryk <phryk@commander1024.de>
2018-06-17 16:26:38 +0200

    added optional grid to plots, started adding stuff for more complex color handling

--- hugin.py
+++ hugin.py
@@ -24,6 +24,206 @@ CONFIG_COLORS = {
 
 ## Helpers ##
 
+class Color(object):
+
+    red = None
+    green = None
+    blue = None
+
+    hue = None
+    saturation = None
+    value = None
+
+    alpha = None
+
+    def __init__(self, red=None, green=None, blue=None, hue=None, saturation=None, value=None, alpha=None):
+
+        rgb_passed = bool(red)|bool(green)|bool(blue)
+        hsv_passed = bool(hue)|bool(saturation)|bool(value)
+
+        if not alpha:
+            alpha = 0.0
+
+        if rgb_passed and hsv_passed:
+            raise ValueError("Color can't be initialized with RGB and HSV at the same time.")
+
+        elif hsv_passed:
+
+            if not hue:
+                hue = 0.0
+            if not saturation:
+                saturation = 0.0
+            if not value:
+                value = 0.0
+
+            super(Color, self).__setattr__('hue', hue)
+            super(Color, self).__setattr__('saturation', saturation)
+            super(Color, self).__setattr__('value', value)
+            self._update_rgb()
+
+        else:
+
+            if not red:
+                red = 0
+            if not green:
+                green = 0
+            if not blue:
+                blue = 0
+
+            super(Color, self).__setattr__('red', red)
+            super(Color, self).__setattr__('green', green)
+            super(Color, self).__setattr__('blue', blue)
+            self._update_hsv()
+
+        super(Color, self).__setattr__('alpha', alpha)
+
+
+    def __setattr__(self, key, value):
+
+        if key in ('red', 'green', 'blue'):
+            if value > 255.0:
+                value = value % 255.0
+            super(Color, self).__setattr__(key, value)
+            self._update_hsv()
+
+        elif key in ('hue', 'saturation', 'value'):
+            if key == 'hue' and (value >= 360.0 or value < 0):
+                value = value % 360.0
+            elif key != 'hue' and value > 1.0:
+                value = 1.0
+            super(Color, self).__setattr__(key, value) 
+            self._update_rgb()
+
+        else:
+            if key == 'alpha' and value > 1.0: # TODO: Might this be more fitting in another place?
+                value = 1.0
+
+            super(Color, self).__setattr__(key, value)
+
+
+    def __repr__(self):
+
+        return '<%s: red %f, green %f, blue %f, hue %f, saturation %f, value %f, alpha %f>' % (
+                self.__class__.__name__,
+                self.red,
+                self.green,
+                self.blue,
+                self.hue,
+                self.saturation,
+                self.value,
+                self.alpha
+            )
+
+
+    def __str__(self):
+        return "%d %d %d" % (
+            int(round(self.red * self.alpha)),
+            int(round(self.green * self.alpha)),
+            int(round(self.blue * self.alpha)),
+        )
+
+
+    def blend(self, other, mode='normal'):
+
+        if self.alpha != 1.0: # no clue how to blend with a translucent bottom layer
+            self.red = self.red * self.alpha
+            self.green = self.green * self.alpha
+            self.blue = self.blue * self.alpha
+
+            self.alpha = 1.0
+
+        if mode == 'normal':
+            own_influence = 1.0 - other.alpha
+            self.red = (self.red * own_influence) + (other.red * other.alpha)
+            self.green = (self.green * own_influence) + (other.green * other.alpha)
+            self.blue = (self.blue * own_influence) + (other.blue * other.alpha)
+
+
+    def lighten(self, other):
+
+        if isinstance(other, int) or isinstance(other, float):
+            other = Color(red=other, green=other, blue=other, alpha=1.0)
+
+        if self.alpha != 1.0:
+            self.red = self.red * self.alpha
+            self.green = self.green * self.alpha
+            self.blue = self.blue * self.alpha
+
+            self.alpha = 1.0
+
+        red = self.red + (other.red * other.alpha)
+        green = self.green + (other.green * other.alpha)
+        blue = self.blue + (other.blue * other.alpha)
+
+        if red > 255.0:
+            red = 255.0
+
+        if green > 255.0:
+            green = 255.0
+
+        if blue > 255.0:
+            blue = 255.0
+
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+
+    def darken(self, other):
+
+        if isinstance(other, int) or isinstance(other, float):
+            other = Color(red=other, green=other, blue=other, alpha=1.0)
+
+        red = self.red - other.red
+        green = self.green - other.green
+        blue = self.blue - other.blue
+
+        if red < 0:
+            red = 0
+
+        if green < 0:
+            green = 0
+
+        if blue < 0:
+            blue = 0
+
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+    
+    def hex_rgb(self):
+        return "%.2X%.2X%.2X" % (self.red, self.green, self.blue)
+    
+    
+    def hex_rgba(self):
+        return "%.2X%.2X%.2X%.2X" % (self.red, self.green, self.blue, self.alpha * 255)
+
+
+    def tuple_rgb(self):
+        """ return color (without alpha) as tuple, channels being float 0.0-1.0 """
+        return (self.red/255.0, self.green/255.0, self.blue/255.0)
+    
+    
+    def tuple_rgba(self):
+        """ return color (*with* alpha) as tuple, channels being float 0.0-1.0 """
+        return (self.red/255.0, self.green/255.0, self.blue/255.0, self.alpha)
+
+
+    def _update_hsv(self):
+
+        hue, saturation, value = colorsys.rgb_to_hsv(self.red/255.0, self.green/255.0, self.blue/255.0)
+        super(Color, self).__setattr__('hue', hue * 360.0)
+        super(Color, self).__setattr__('saturation', saturation)
+        super(Color, self).__setattr__('value', value)
+
+
+    def _update_rgb(self):
+
+        red, green, blue = colorsys.hsv_to_rgb(self.hue / 360.0, self.saturation, self.value)
+        super(Color, self).__setattr__('red', red * 255.0)
+        super(Color, self).__setattr__('green', green * 255.0)
+        super(Color, self).__setattr__('blue', blue * 255.0)
 def pretty_bytes(bytecount):
 
     """
@@ -594,6 +794,11 @@ class PlotGauge(Gauge):
 
 
     @property
+    def scale_factor(self):
+        return max(self.points)
+
+
+    @property
     def points_scaled(self):
         
         scale_factor = max(self.points)
@@ -608,14 +813,35 @@ class PlotGauge(Gauge):
         return r
 
 
+    def draw_grid(self, context, monitor):
+
+        context.set_line_width(1)
+        context.set_source_rgba(1,1,1, 0.1)
+        #context.set_dash([1,1])
+
+        for x in range(self.x + self.padding, self.x + self.padding + self.inner_width, 8):
+            context.move_to(x, self.y + self.padding)
+            context.line_to(x, self.y + self.padding + self.inner_height)
+
+        for y in range(self.y + self.padding, self.y + self.padding + self.inner_height, 8):
+            context.move_to(self.x + self.padding, y)
+            context.line_to(self.x + self.padding + self.inner_width, y)
+
+        context.stroke()
+        #context.set_dash([1,0]) # reset dash
+
+
     def update(self, context, monitor):
             
         self.points.append(monitor.normalized(self.address))
-        
+      
+        #TODO: if self.background?
         context.set_line_width(2)
         context.set_source_rgba(*CONFIG_COLORS['gauge_background'])
         context.rectangle(self.x + self.padding, self.y + self.padding, self.inner_width, self.inner_height)
         context.fill()
+
+        self.draw_grid(context, monitor)
 
         coords = []
         if self.autoscale:
@@ -835,10 +1061,10 @@ hugin.gauges['network'] = [
         y=600,
         width=hugin.window.width,
         height=hugin.window.width,
-        address=['re0.bytes_recv', 're0.bytes_sent'],
+        address=['em0.bytes_recv', 'em0.bytes_sent'],
         captions=[
             {
-                'text': '{re0[counters][bytes_recv]}/s\n{re0[counters][bytes_sent]}/s',
+                'text': '{em0[counters][bytes_recv]}/s\n{em0[counters][bytes_sent]}/s',
                 'position': 'center_center',
                 'align': 'center_center',
             }
@@ -851,7 +1077,7 @@ hugin.gauges['network'] = [
         width=hugin.window.width,
         height=100,
         padding=15,
-        address='re0.bytes_recv'
+        address='em0.bytes_recv'
     ),
     
     PlotGauge(
@@ -860,7 +1086,7 @@ hugin.gauges['network'] = [
         width=hugin.window.width,
         height=100,
         padding=15,
-        address='re0.bytes_sent'
+        address='em0.bytes_sent'
     )
 ]
 
